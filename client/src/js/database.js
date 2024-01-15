bac086f04b1f906f093e8ba5a0c4f492b255e82a
Vishal <vishal.manglani7@gmail.com>
2024-01-14 21:36:19 -0500

    finished todos on db and webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+//create connenction to db
+const textDb = await openDB('jate', 1);
+//creates a new tx which is read only
+const tx = jateDb.transaction('jate', 'readonly');
+//open objectstore
+const store = tx.objectStore('jate');
+//get all data from db
+const request = store.getAll();
+//confirm request
+const result = await request;
+return result.value;};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {console.error('getDb not implemented');
+const jateDb = await openDB('jate', 1);
+const tx = jateDb.transaction('jate', 'readwrite');
+const store = tx.objectStore('jate');
+const request = store.put({ id: 1, value: content });
+const result = await request;
+console.log(result);
+};
+initdb();
+
 
 initdb();
